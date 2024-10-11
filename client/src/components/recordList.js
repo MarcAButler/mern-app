735c4d42f3204210b3c8e73bca63e6affe69daa9
MarcAButler <60492020+MarcAButler@users.noreply.github.com>
2024-10-11 10:23:57 -0400

    Update recordList.js

--- client/src/components/recordList.js
+++ client/src/components/recordList.js
@@ -27,7 +27,7 @@ export default function RecordList() {
 
     useEffect(() => {
         async function getRecords() {
-            const response = await fetch(`${process.env.REACT_APP_YOUR_HOSTNAME}/record/`)
+            const response = await fetch("localhost:5000/record/")
 
             if (!response.ok) {
                 const message = `An error occurred: ${response.statusText}`
