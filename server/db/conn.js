e10de52841fc9f43f4bab9aee3271d1ebd115a80
MarcAButler <60492020+MarcAButler@users.noreply.github.com>
2024-10-11 09:52:41 -0400

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,26 +1,26 @@
-const { MongoClient } = require("mongodb")
-const Db = process.env.ATLAS_URI
-const client = new MongoClient(Db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+// const { MongoClient } = require("mongodb")
+// const Db = process.env.ATLAS_URI
+// const client = new MongoClient(Db, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
 
-var _db
+// var _db
 
-module.exports = {
-    connectToMongoDB: async function (callback) {
-        try {
-            await client.connect()
-            _db = client.db("employees")
-            console.log("Successfully connected to MongoDB.")
+// module.exports = {
+//     connectToMongoDB: async function (callback) {
+//         try {
+//             await client.connect()
+//             _db = client.db("employees")
+//             console.log("Successfully connected to MongoDB.")
             
-            return callback(null)
-        } catch (error) {
-            return callback(error)
-        }
-    },
+//             return callback(null)
+//         } catch (error) {
+//             return callback(error)
+//         }
+//     },
 
-    getDb: function () {
-        return _db
-    }
-}
+//     getDb: function () {
+//         return _db
+//     }
+// }
