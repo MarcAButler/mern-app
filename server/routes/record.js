2407e6e0f53b17c13032b020573d1430f955e123
MarcAButler <60492020+MarcAButler@users.noreply.github.com>
2024-10-11 10:03:56 -0400

    Update record.js

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,13 +1,9 @@
 const express = require("express")
 const recordRoutes = express.Router()
-// const dbo = require("../db/conn")
-// const ObjectId = require("mongodb").ObjectId
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(async function (req, res) {
-    // const db_connect = dbo.getDb()
     try {
-        // const result = await db_connect.collection("records").find({}).toArray()
         res.status(200).json("HELLO WORLD")
     } catch (error) {
         res.status(404).json({ message: error.message })
